25cf81125d6d7bd769c0f04f5d2845e0810c20b1
usher-0322 <329215298@qq.com>
2026-08-18 09:37:20 +0800

    Add Supabase configuration template

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+// Fill these values after creating your Supabase project.
+// The anon key is designed to be public in browser apps; Row Level Security protects user data.
+window.JP_STUDY_SUPABASE = {
+  url: '',
+  anonKey: ''
+};
