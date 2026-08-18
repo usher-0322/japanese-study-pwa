773770626683585556eb9aebb624c96d6f00a04c
usher-0322 <329215298@qq.com>
2026-08-18 10:09:54 +0800

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 // Fill these values after creating your Supabase project.
 // The anon key is designed to be public in browser apps; Row Level Security protects user data.
 window.JP_STUDY_SUPABASE = {
-  url: 'https://qmrpoyjfjzrnzjzajmbr.supabase.co/rest/v1/',
+  url: 'https://qmrpoyjfjzrnzjzajmbr.supabase.co',
   anonKey: 'sb_publishable_zrYsT7w7qgTaqJLnlJx9BA_ih4nAKmt'
 };
