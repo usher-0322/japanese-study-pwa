1ebe99065277771b01c8e2b3202657038fcc09df
usher-0322 <329215298@qq.com>
2026-08-18 09:59:23 +0800

    Set Supabase URL and anon key
    Updated Supabase configuration with new URL and anon key.

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 // Fill these values after creating your Supabase project.
 // The anon key is designed to be public in browser apps; Row Level Security protects user data.
 window.JP_STUDY_SUPABASE = {
-  url: '',
-  anonKey: ''
+  url: 'sb_publishable_zrYsT7w7qgTaqJLnlJx9BA_ih4nAKmt',
+  anonKey: 'sb_publishable_zrYsT7w7qgTaqJLnlJx9BA_ih4nAKmt'
 };
