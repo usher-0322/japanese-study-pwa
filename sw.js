f6c42afc08f65e1f96963eef91ed45b74552bda4
usher-0322 <329215298@qq.com>
2026-08-23 12:20:46 +0800

    Remove duplicate daily recommendations section

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='jp-study-v16';
+const CACHE='jp-study-v17';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png','./supabase-config.js'];
 self.addEventListener('install',e=>e.waitUntil(Promise.all([caches.open(CACHE).then(c=>c.addAll(ASSETS)),self.skipWaiting()])));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
